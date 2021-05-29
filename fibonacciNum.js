ba070bc4207a332df3833b02c1137003564cd0e9
Cetince <cmstf001@gmail.com>
2021-05-29 15:23:55 +0200

    first

---
+++ fibonacciNum.js
@@ -0,0 +1,10 @@
+function fibonacci(myNumber){
+    let arr = [0, 1]
+    for(let i = 2; i<=myNumber+1; i++){
+        arr.push(arr[i-1] + arr[i-2])
+    }
+    return arr[arr.length - 1] + /n arr;
+}
+let myNumber = parseInt(prompt("Type a number"))
+let fibonacciNumber = fibonacci(myNumber)
+console.log(fibonacciNumber)
