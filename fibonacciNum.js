47d90f84c220de442c2525a8f5da4f7ef9dae41a
Cetince <cmstf001@gmail.com>
2021-05-29 15:26:46 +0200

    second

--- fibonacciNum.js
+++ fibonacciNum.js
@@ -3,7 +3,7 @@ function fibonacci(myNumber){
     for(let i = 2; i<=myNumber+1; i++){
         arr.push(arr[i-1] + arr[i-2])
     }
-    return arr[arr.length - 1] + /n arr;
+    return arr[arr.length - 1]
 }
 let myNumber = parseInt(prompt("Type a number"))
 let fibonacciNumber = fibonacci(myNumber)
